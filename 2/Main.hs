39f07741c58f37bfd16166720731306a0ad44991
Andreas Ekeroot <andeke@gmail.com>
2019-12-02 19:32:09 +0100

    Day 2, part 1 done

--- 2/Main.hs
+++ 2/Main.hs
@@ -1,33 +1,40 @@
 {-# LANGUAGE LambdaCase #-}
 import           Control.Arrow                            ( (***) )
+import           Control.Monad                            ( (>=>) )
+import           Control.Monad.Primitive                  ( PrimMonad
+                                                          , PrimState
+                                                          )
 import           Data.List                                ( break
                                                           , uncons
                                                           , unfoldr
                                                           )
 import qualified Data.Vector.Unboxed           as VU
+import qualified Data.Vector.Unboxed.Mutable   as VUM
 
-doubleRead :: _
-doubleRead v = VU.read v >=> VU.read v
+doubleRead :: PrimMonad m => VUM.MVector (PrimState m) Int -> Int -> m Int
+doubleRead v = VUM.read v >=> VUM.read v
 
-binHelper :: (Int -> Int -> Int) -> _ -> Int -> _
+binHelper
+  :: PrimMonad m
+  => (Int -> Int -> Int)
+  -> VUM.MVector (PrimState m) Int
+  -> Int
+  -> m ()
 binHelper f v pc = (f <$> doubleRead v (pc + 1) <*> doubleRead v (pc + 2))
-  >>= \r -> VU.read v (pc + 2) >>= \dest -> VU.write v dest r
+  >>= \r -> VUM.read v (pc + 3) >>= \dest -> VUM.write v dest r
 
-runInstruction :: _
-runInstruction v pc =
-  VU.read v pc
-    >>= \case
-          1  -> binHelper (+) v pc
-          2  -> binHelper (*) v pc
-          99 -> error "DONE!"
-    >>= (`runInstruction` (pc + 4))
+eval :: PrimMonad m => Int -> VUM.MVector (PrimState m) Int -> m ()
+eval pc v = VUM.read v pc >>= \case
+  1  -> binHelper (+) v pc >> eval (pc + 4) v
+  2  -> binHelper (*) v pc >> eval (pc + 4) v
+  99 -> pure ()
+  i  -> error $ "Nope: " <> show i
 
 main = do
   program <-
-    VU.fromList
-    .   unfoldr
-          (sequence . ((read :: String -> Int) *** fmap snd . uncons) . break
-            (== ',')
-          )
+    VU.modify (eval 0)
+    .   VU.modify (\v -> VUM.write v 1 12 >> VUM.write v 2 2)
+    .   VU.fromList
+    .   unfoldr (sequence . (read *** fmap snd . uncons) . break (== ','))
     <$> readFile "input.txt"
   print program
