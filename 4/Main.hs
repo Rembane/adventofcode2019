00c02a955803aa3eb9668a2a5e6b30a097a99db6
Andreas Ekeroot <andeke@gmail.com>
2019-12-06 11:29:48 +0100

    Solve day 4, part 1

---
+++ 4/Main.hs
@@ -0,0 +1,15 @@
+import           Data.Char                                ( digitToInt )
+import           Data.List                                ( foldl'
+                                                          , group
+                                                          )
+doubleDigit :: String -> Bool
+doubleDigit = any ((> 1) . length) . group
+
+neverDecrease :: String -> Bool
+neverDecrease = snd . foldl'
+  (\(pre, b) c -> let i = digitToInt c in (i, i >= pre && b))
+  (0, True)
+
+main = print $ length $ filter
+  (\i -> let s = show i in doubleDigit s && neverDecrease s)
+  [168630 .. 718098]
