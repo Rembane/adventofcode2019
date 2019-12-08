19d18853ff89ee751398b529a4f597a1610aa70e
Andreas Ekeroot <andeke@gmail.com>
2019-12-08 21:18:44 +0100

    Day 8, part 2

--- 8/Main.hs
+++ 8/Main.hs
@@ -8,12 +8,24 @@ layerize :: String -> [String]
 layerize [] = []
 layerize xs = uncurry (:) $ second layerize $ splitAt 150 xs
 
-main =
-  print
-    .   product
-    .   tail
-    .   minimumBy (comparing head)
-    .   map (map length . group . sort)
-    .   layerize
-    .   filter isDigit
-    =<< readFile "input.txt"
+part1 :: [String] -> Int
+part1 =
+  product . tail . minimumBy (comparing head) . map (map length . group . sort)
+
+part2 :: [String] -> String
+part2 []   = []
+part2 [ly] = ly
+part2 (ly : lys) =
+  zipWith (\c1 c2 -> if c1 == '2' then c2 else c1) ly (part2 lys)
+
+main :: IO ()
+main = do
+  layers <- layerize . filter isDigit <$> readFile "input.txt"
+  print $ part1 layers
+  mapM_ print
+    $ unfoldr
+        (\s ->
+          let (s', r) = splitAt 25 s
+          in  if null s' then Nothing else Just (s', r)
+        )
+    $ map (\c -> if c == '0' then ' ' else c) (part2 layers)
