f213db11f5a44d15ba0145738e1ce2e32f679461
Andreas Ekeroot <andeke@gmail.com>
2019-12-12 16:43:49 +0100

    Day 10: Part 2, this is frustrating, try again later!

--- 10/Main.hs
+++ 10/Main.hs
@@ -4,13 +4,19 @@ import           Control.Arrow                            ( (***)
                                                           , (&&&)
                                                           )
 import           Data.Function                            ( on )
-import           Data.List                                ( group
+import           Data.List                                ( break
+                                                          , group
                                                           , groupBy
                                                           , maximumBy
                                                           , sort
                                                           , sortOn
                                                           )
 import           Data.Ord                                 ( comparing )
+import qualified Data.Sequence                 as Sq
+import           Data.Sequence                            ( Seq(..)
+                                                          , (|>)
+                                                          )
+import           Data.Tuple                               ( swap )
 
 both :: (a -> b) -> (a, a) -> (b, b)
 both f = f *** f
@@ -18,13 +24,27 @@ both f = f *** f
 tt :: ((a, b), (a, b)) -> ((a, a), (b, b))
 tt ((x1, y1), (x2, y2)) = ((x1, x2), (y1, y2))
 
+calcAngle :: (Double, Double) -> (Double, Double) -> Double
+calcAngle p = uncurry (flip atan2) . both (uncurry (-)) . tt . (, p)
+
 seenAsteroids
   :: (Double, Double) -> [(Double, Double)] -> ((Double, Double), Int)
-seenAsteroids p = (p, ) . length . group . sort . map
-  (uncurry (flip atan2) . both (uncurry (-)) . tt . (, p))
+seenAsteroids p = (p, ) . length . group . sort . map (calcAngle p)
 
 toDouble :: Int -> Double
 toDouble = fromIntegral
+
+consume :: Int -> Seq [(Double, Double)] -> (Double, Double)
+consume _ Empty      = error "This should never happen."
+consume n (x :<| xs) = case horiz n x of
+  (200, [x']) -> x'
+  (n' , []  ) -> consume n' xs
+  (n' , x'  ) -> consume n' (xs |> x')
+ where
+  horiz :: Int -> [(Double, Double)] -> (Int, [(Double, Double)])
+  horiz n   []       = (n, [])
+  horiz 200 (x : xs) = (200, [x])
+  horiz n   (x : xs) = (succ n, xs)
 
 main :: IO ()
 main = do
@@ -40,6 +60,24 @@ main = do
     <$> readFile "input.txt"
 
   -- Part 1
-  let station =
+  let (station, seen) =
         maximumBy (comparing snd) $ map (`seenAsteroids` asteroids) asteroids
-  print station
+  print seen
+
+  -- Part 2
+  -- This list goes counter clockwise, we want to go clockwise, so we reverse the list.
+  -- We also want to go from (0,1) and not (1,0) which atan2 does, so we start from pi/2
+  -- instead of 0.
+  print
+    . (\(x, y) -> x * 100 + y)
+    . consume 1
+    . Sq.fromList
+    . map (sortOn (\(x, y) -> sqrt $ x ** 2.0 + y ** 2.0) . map fst)
+    . uncurry (++)
+    . swap
+    . break ((<= (pi / 2)) . snd . head)
+    . reverse
+    . groupBy (on (==) snd)
+    . sortOn snd
+    . map (id &&& calcAngle station)
+    $ asteroids
